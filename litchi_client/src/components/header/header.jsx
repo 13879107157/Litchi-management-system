2a34cf8ab26ecff97cb13c16911ba2abde49a807
Chentenghui <1412337876@qq.com>
2019-12-31 13:53:03 +0800

    优化header组件

--- litchi_client/src/components/header/header.jsx
+++ litchi_client/src/components/header/header.jsx
@@ -2,75 +2,93 @@ import React, { Component } from 'react';
 import Cookies from 'js-cookie';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
-import {Layout,Modal} from 'antd';
+import {Layout,Modal,Button} from 'antd';
 
 import './index.less'
 import {getWeather} from '../../redux/action'
+import menuList  from '../../config/menuConfig'
 const { Header } = Layout;
 class Heade extends Component {
-    state = {
-        visible: false
+  state = {
+      visible: false
+  };
+  UNSAFE_componentWillMount(){
+    this.props.getWeather()
+  }
+  handleOk = () => {
+      Cookies.remove('userid')
+      this.props.history.replace('/login')
+      this.setState({
+        visible: false,
+      });
     };
-    handleOk = () => {
-        Cookies.remove('userid')
-        this.props.history.replace('/login')
-        this.setState({
-          visible: false,
-        });
-      };
-      handleCancel = () => {
-        this.setState({
-          visible: false,
-        });
-      };
-      showModal = () => {   //退出对话框
-        this.setState({
-          visible: true,
-        });
-      };
-      UNSAFE_componentWillMount(){
-        this.props.getWeather()
-      }
-
-    render() {
-        const {weather} = this.props
-        if(weather === 0){
-            return null
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+  showModal = () => {   //退出对话框
+    this.setState({
+      visible: true,
+    });
+  };
+  getTitle = () => {
+    const path = this.props.history.location.pathname   //当前路径
+    let title
+    menuList.forEach(item => {
+      if(item.key === path){
+        title = item.title
+      } else if(item.children){
+        const cItem = item.children.find(cItem => cItem.key === path)
+        if(cItem){
+          title = cItem.title
         }
-        //console.log(weather);
-        
-        const {wea,wea_img} = weather
-        
-        return (
-            <Header className="header">
-                <div className="header-top">
+      }
+    })
+    return title
+  }
+  render() {
+      const {weather} = this.props
+      if(weather === 0){
+          return null
+      }
+      //console.log(weather);
+      const {wea,wea_img,date} = weather
+      const title = this.getTitle()
+      //console.log(title);
+      
+      return (
+          <Header className="header">
+              <div className="header-top">
+                  <span>
                     欢迎{this.props.username}
-                    &nbsp;&nbsp;&nbsp;
-                    <span onClick={this.showModal} type="danger">退出</span>
-                </div>
-                <hr/>
-                <div className="header-bottom">
-                    <div className="header-bottom-left">首页</div>
-                    <div className="header-bottom-right">
-                        <span>2019-12-31 9:50:00</span>
-                        <img src={require(`../../assets/imges/weather/${wea_img}.png`)} alt=""/>
-                        <span>{wea}</span>
-                    </div>
-                </div>
+                  </span>
+                  &nbsp;&nbsp;&nbsp;
+                  <Button className="logout"  type="danger" onClick={this.showModal}>退出</Button>
+              </div>
+              <hr/>
+              <div className="header-bottom">
+                  <div className="header-bottom-left">{title}</div>
+                  <div className="header-bottom-right">
+                      <span>{date}</span>
+                      <img src={require(`../../assets/imges/weather/${wea_img}.png`)} alt=""/>
+                      <span>{wea}</span>
+                  </div>
+              </div>
 
-                <Modal
-                    visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
-                    centered={true}
-                    mask={true}  
-                >
-                <h1>确定要退出登陆吗</h1>
-              </Modal>
-            </Header>
-            
-        );
-    }
+              <Modal
+                  visible={this.state.visible}
+                  onOk={this.handleOk}
+                  onCancel={this.handleCancel}
+                  centered={true}
+                  mask={true}  
+              >
+              <h1>确定要退出登陆吗</h1>
+            </Modal>
+          </Header>
+          
+      );
+  }
 }
 
 export default connect(
